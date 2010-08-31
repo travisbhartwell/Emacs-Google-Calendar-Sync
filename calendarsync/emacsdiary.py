03d83069303288d3f2bfb1ed14cc724039e5caca
Travis B. Hartwell <nafai@travishartwell.net>
2010-08-30 18:17:54 -0600

    Start modules in calendarsync package.

---
+++ calendarsync/emacsdiary.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2008-2010 by CiscoRx
+# Copyright (C) 2010 by Travis B. Hartwell
+#
+# This file is part of Emacs Google Calendar Sync
+#
+# Emacs Google Calendar Sync is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Emacs Google Calendar Sync is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Emacs Google Calendar Sync.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+"""Functions to support parsing and writing the Emacs Diary file."""
---
+++ calendarsync/sync.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2008-2010 by CiscoRx
+# Copyright (C) 2010 by Travis B. Hartwell
+#
+# This file is part of Emacs Google Calendar Sync
+#
+# Emacs Google Calendar Sync is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Emacs Google Calendar Sync is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Emacs Google Calendar Sync.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+"""Synchronization between Emacs Diary and Google Calendar, functions
+tying things together.
+"""
+
+
+
+def run_synchronization(configuration):
+    """The top-level function that performs the synchronization based
+    on the passed in configuration."""
+
+    print "Running synchronization of Google Calendar and Emacs Diary."
+
+
